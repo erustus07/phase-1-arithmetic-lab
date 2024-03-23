8a95080eb777e4723419c18e1f89b2c390c58983
erustus07 <erustusndegwa08@gmail.com>
2024-03-23 21:04:20 +0300

    arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
 
-// Write your code here
+
+      // Find two numbers that multiply to 62
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+console.log(multiply); // This will output 62  
+
+     
+
+
+
+      //random number between 1 and 10
+const random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+
+
+
+    //division whose answer is 4
+    const num3 = 24;
+const num4 = 5;
+const mod = ((num3 % num4) + num4) % num4;
+console.log(mod); // Output: 4
+
+
+      //where max =20
+let max =20;
+let numb1 = 15;
+let numb2 = 10;
+let numb3 = 5;
+let numb4 = 20;
+ if(numb1 > numb2) {max = numb1;}
+ if(numb2 > numb3) {max = numb2;}
+
+ if(numb4 > numb1) {max = numb4;}
+ 
+ console.log(max);
